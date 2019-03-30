c5bca36262743f60f3ade791a8a13c7453e8ad6c
dncrash <dncrash@gmail.com>
2019-03-30 18:53:43 +0200

    added real English PDF

--- VFDTube2/examples/SimpleClock/SimpleClock.ino
+++ VFDTube2/examples/SimpleClock/SimpleClock.ino
@@ -9,43 +9,44 @@ VFDTube2 tube(8, 9, 10, 11, TUBE_COUNT);
 
 void setup()
 {
-	tube.setBrightness(0xc0); // set brightness, range 0x00 - 0xff
+    tube.setBrightness(0xc0); // set brightness, range 0x00 - 0xff
+    // Serial.begin(9600); // enable for troubleshooting on Serial port
 
 }
 
 // display a basic digital clock pattern
 void loop()
 {
-	static byte s = 56; // second
-	static byte m = 34; // minute
+    static byte s = 56; // second
+    static byte m = 34; // minute
 
-	tube.clear();
-//	tube.setBackgroundColor(Red);
-	tube.setBackgroundColor((Color) ((s) / 7));
-	// background color shift in every 7 seconds
+    tube.clear();
+    tube.setBackgroundColor(White);
+    // tube.setBackgroundColor((Color) ((s) / 7));
+    // background color shift in every 7 seconds
 
-	if (s & 0x01)
-		tube.printf("%02d%02d.", m, s);
-	else
-		tube.printf("%02d%02d", m, s);
+    if (s & 0x01)
+        tube.printf("%02d%02d.", m, s);
+    else
+        tube.printf("%02d%02d", m, s);
 
-	//tube.setPoint(3);
+    // tube.setPoint(3);
 
-	tube.display();
-	delay(1000); // reduce this value to check a fast shifting
+    tube.display();
+    // Serial.print(s); // enable for troubleshooting on Serial port
+    delay(1000); // reduce this value to check a fast shifting
 
-	s++;
+    s++;
 
-	if (s == 60)
-	{
-		s = 0;
-		m++;
-	}
-	if (m == 60)
-	{
-		m = 0;
+    if (s == 60)
+    {
+        s = 0;
+        m++;
+    }
+    if (m == 60)
+    {
+        m = 0;
 
-	}
+    }
 
 }
-
